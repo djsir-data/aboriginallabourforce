9973e3b637dd56b70141e66d3fbaf9a3a0cde3a6
Henry McMillan <henry.mcmillan@ecodev.vic.gov.au>
2023-05-29 19:06:57 +1000

    initial commit

---
+++ data-raw/clean_census.R
@@ -0,0 +1,39 @@
+library(dplyr)
+library(readr)
+library(stringr)
+library(tidyr)
+
+census <- read_csv("data-raw/Vic ATSI Employment SA3 .csv", skip = 10, )
+names(census) <- c("drop1", "drop2", "ind", "lf_stat", "sa3", "count")
+
+census <- census %>% 
+  select(-contains("drop")) %>% 
+  filter(!is.na(count)) %>%
+  mutate(
+    ind = ifelse(ind == "Non-Indigenous", ind, "ATSI"),
+    lf_stat = str_extract(
+      lf_stat, 
+      "Employed|Unemployed|Not in the labour force"
+      ) %>% 
+      str_to_lower() %>% 
+      str_replace("not in the labour force", "nilf")
+  ) %>%
+  pivot_wider(names_from = lf_stat, values_from = count, values_fn = sum) 
+
+
+census_total <- census %>% 
+  group_by(sa3) %>% 
+  summarise(across(where(is.numeric), sum)) %>% 
+  ungroup() %>% 
+  mutate(ind = "total")
+
+
+census <- census %>% 
+  bind_rows(census_total) %>% 
+  mutate(
+    unemp_rate = unemployed / (employed + unemployed),
+    part_rat = (employed + unemployed) / (employed + unemployed + nilf),
+    emp_pop = employed / (employed + unemployed + nilf)
+    )
+
+write_csv(census, "data/census.csv")
